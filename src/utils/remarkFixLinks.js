453b8701742a2df28f5e7a6c649c587ada4f73c6
Eugenio Pace <eugenio.pace@gmail.com>
2025-06-07 09:33:46 -0700

    fixed remark filter for image routes

--- src/utils/remarkFixLinks.js
+++ src/utils/remarkFixLinks.js
@@ -4,10 +4,28 @@ import { visit } from 'unist-util-visit'
 export default function remarkFixLinks() {
   return (tree) => {
     const base = import.meta.env.BASE_URL || '/'
-    visit(tree, 'link', (node) => {
-      if (node.url.startsWith('/')) {
+
+    visit(tree, (node) => {
+      // Handle links
+      if (node.type === 'link' && node.url.startsWith('/')) {
+        node.url = base.replace(/\/$/, '') + node.url
+      }
+
+      // Handle images
+      if (node.type === 'image' && node.url.startsWith('/')) {
         node.url = base.replace(/\/$/, '') + node.url
       }
     })
   }
 }
+
+// export default function remarkFixLinks() {
+//   return (tree) => {
+//     const base = import.meta.env.BASE_URL || '/'
+//     visit(tree, 'link', (node) => {
+//       if (node.url.startsWith('/')) {
+//         node.url = base.replace(/\/$/, '') + node.url
+//       }
+//     })
+//   }
+// }
