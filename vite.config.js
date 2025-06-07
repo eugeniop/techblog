4428d06411ec05f3daf82ff1cbd85e211eca0cb8
Eugenio Pace <eugenio.pace@gmail.com>
2025-06-06 17:31:11 -0700

    Disabled Jekyll for GH pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import generatePostsPlugin from './vite.plugins.generatePosts.js'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/techblog/' : '/',
   plugins: [
     generatePostsPlugin(),
     react()],
