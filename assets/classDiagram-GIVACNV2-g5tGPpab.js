f40a5375c78a05b337098f1ebb84e9b0790674f7
Eugenio Pace <eugenio.pace@gmail.com>
2025-06-06 18:40:21 -0700

    Updates

---
+++ assets/classDiagram-GIVACNV2-g5tGPpab.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-BAX__Hrb.js";import{_ as l}from"./index-cQlxhPCf.js";import"./chunk-RZ5BOZE2-Cvk85TXg.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
